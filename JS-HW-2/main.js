97cd676ece4801f926a5e5778daa400d2517527e
Naim AK <naimak659@gmail.com>
2024-03-29 16:36:52 +0600

    js task 2 created & data types are assinged

---
+++ JS-HW-2/main.js
@@ -0,0 +1,15 @@
+// My Data
+let Name = "naim", //string
+  ID = 54654614,
+  Department = "Web & software",
+  Batch = "MERN stack 2307",
+  mentor = "TaufiK islam",
+  Regular = true,
+  TotalPresentClass = "0.96",
+  VarsityName = null;
+
+console.log(typeof Name);
+console.log(typeof ID);
+console.log(typeof Regular);
+console.log(typeof TotalPresentlass);
+console.log(typeof VarsityName);
