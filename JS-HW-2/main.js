3ec7b99563ea64f4d1e9e84a0c3ae1f04a26fa57
Naim AK <naimak659@gmail.com>
2024-03-30 12:04:49 +0600

    HW-2 is done

--- JS-HW-2/main.js
+++ JS-HW-2/main.js
@@ -1,15 +1,54 @@
 // My Data
 let Name = "naim", //string
-  ID = 54654614,
+  ID = 54654614, //number
   Department = "Web & software",
   Batch = "MERN stack 2307",
   mentor = "TaufiK islam",
-  Regular = true,
-  TotalPresentClass = "0.96",
-  VarsityName = null;
+  Regular = true, //Boolean
+  TotalPresentClass = 0.98,
+  VarsityName = null, // NULL
+  ok;
 
+// DataType of My Data
 console.log(typeof Name);
 console.log(typeof ID);
 console.log(typeof Regular);
-console.log(typeof TotalPresentlass);
+console.log(typeof TotalPresentClass);
 console.log(typeof VarsityName);
+console.log(typeof ok);
+
+// Oparator
+
+let number1 = 23;
+let number2 = 12;
+let number3 = 5;
+let addition;
+const pi = 3.141592653589793238462643383279502884197;
+
+// number2 is powered by 2
+console.log("The square of number1 is", number2 ** 2);
+
+// remainder of number1 by 4
+console.log("The remainder of number1 is", number1 % 4);
+
+// decrement of number1
+console.log("Increment of number1", --number1); //  remember from now number1 = 22
+
+// increment of number2
+console.log("Increment of number2", ++number1); //  remember from now number1 = 22
+
+// addition of number1  and number2
+console.log("addition by using assignment",number1 += number2);
+
+// Submission of number1  and number2
+console.log("submission by using assignment",number3 -= number2);
+
+// Multiplication
+let number4 = 2;
+let number5 = 6;
+console.log("multiplication by using assignment",number4 *= number5);
+
+// division
+let number6 = 8;
+console.log("division by using assignment",number6 /= number5);
+
