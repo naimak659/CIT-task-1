7c2b5133c88e3e3d095fa58a8de62658ee8cb1e9
Naim AK <naimak659@gmail.com>
2024-05-06 12:53:58 +0600

    hw 8 is done

--- 8-Js-HW/main.js
+++ 8-Js-HW/main.js
@@ -61,3 +61,17 @@ console.log(
 for (const key in blank) {
   console.log(`key: ${key} & property: ${blank[key]}`);
 }
+let codes = {
+  49: "Germany",
+  41: "Switzerland",
+  44: "Great Britain",
+  // ..,
+  1: "USA",
+};
+
+for (let code in codes) {
+  console.log(code); // 1, 41, 44, 49
+}
+
+
+delete blank.name
