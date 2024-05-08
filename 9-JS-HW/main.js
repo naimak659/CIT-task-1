ea31556d89ef7093cd1af89a4eaa1b8c69c68afc
Naim AK <naimak659@gmail.com>
2024-05-08 10:18:05 +0600

    added some comments in 9-HW

--- 9-JS-HW/main.js
+++ 9-JS-HW/main.js
@@ -9,12 +9,14 @@ function createNav() {
   nav.appendChild(img);
   createLinks(nav);
 
+  // navbar styling
   nav.style.backgroundColor = "#1C1E53";
   nav.style.padding = "16px 48px";
   nav.style.display = "flex";
   nav.style.justifyContent = "space-between";
   nav.style.alignItems = "center";
 
+  // apending navbar in root
   root.appendChild(nav);
 }
 
@@ -22,11 +24,18 @@ function createNav() {
 function createLinks(parent) {
   let navLink = ["Home", "About us", "Feature", "Pricing", "FAQ", "Blog"];
   let div = document.createElement("div");
+
+  // Creating the link using for loop
   for (let i = 0; i < navLink.length; i++) {
     let a = document.createElement("a");
+    // * set attribute of a
     a.setAttribute("href", `#`);
-    // a.setAttribute("href", `/${navLink[i]}`);
+
+    // styling the a tag
+    // TODO: a.setAttribute("href", `/${navLink[i]}`);
     a.innerHTML = navLink[i];
+
+    // *styling a tag if it is active or not
     if (i == 0) {
       a.style.color = "white";
     } else {
@@ -44,7 +53,10 @@ function createLinks(parent) {
 function createBTN(parent) {
   let btn = document.createElement("button");
   let div = document.createElement("div");
+
   btn.textContent = "Contact us";
+
+  // btn styling
   btn.style.padding = "8px 28px";
   btn.style.color = "white";
   btn.style.textAlign = "center";
@@ -53,9 +65,10 @@ function createBTN(parent) {
   btn.style.backgroundColor = "transparent";
   btn.style.border = "2px solid rgba(244, 246, 252, 0.2)";
   btn.style.marginLeft = "16px";
+
+  // btn appendding in div
   div.appendChild(btn);
   parent.appendChild(div);
 }
 
 createNav();
-console.log(root);
